30057123b0b00e55a24544cc4c14865b9f667f18
samerunet <samerunet@gmail.com>
2022-08-31 11:22:03 -0700

    patch change

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post("/create", (req, res) => {
 	});
 });
 
-app.patch("/update/:id", (req, res) => {
+app.put("/update/:id", (req, res) => {
 	userSchema.findByIdAndUpdate(
 		req.params.id,
 		req.body,
